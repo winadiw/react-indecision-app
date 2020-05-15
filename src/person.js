2d8a21555437f7e2d79c8962261613a8baa2fc7c
Winadi <winadiw@gmail.com>
2020-05-15 20:25:07 +0700

    #51 - simple webpack, dummy app.js, import export

---
+++ src/person.js
@@ -0,0 +1,7 @@
+export const isAdult = (age) => {
+    return age >= 18;
+}
+
+export const canDrink = (age) => {
+    return age >= 21
+}
---
+++ src/utils.js
@@ -0,0 +1,9 @@
+console.log('utils.js is running');
+
+export const square = (x) => x * x;
+export const add = (x, y) => x + y;
+
+// export {
+//     square,
+//     add
+// };
