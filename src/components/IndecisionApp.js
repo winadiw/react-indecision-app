e3e52dd81a2ca7b3f8ebf7a7a0834a9877d05a62
Winadi <winadiw@gmail.com>
2020-05-15 21:33:18 +0700

    #58 - ES6 class properties using babel transform-class-properties for es6 arrow and binding 'this'

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -5,17 +5,9 @@ import Header from "./Header";
 import Action from "./Action";
 
 export default class IndecisionApp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
-    this.handlePick = this.handlePick.bind(this);
-    this.handleAddOption = this.handleAddOption.bind(this);
-    this.handleDeleteOption = this.handleDeleteOption.bind(this);
-
-    this.state = {
-      options: props.options,
-    };
-  }
+  state = {
+    options: this.props.options
+  };
 
   componentDidMount() {
     try {
@@ -41,23 +33,23 @@ export default class IndecisionApp extends React.Component {
     console.log("componentWillUnmount!");
   }
 
-  handleDeleteOptions() {
+  handleDeleteOptions = () => {
     this.setState(() => ({ options: [] }));
   }
 
-  handleDeleteOption(optionToRemove) {
+  handleDeleteOption = (optionToRemove) => {
     this.setState((prevState) => ({
       options: prevState.options.filter((option) => optionToRemove !== option),
     }));
   }
 
-  handlePick() {
+  handlePick = () => {
     const randomNum = Math.floor(Math.random() * this.state.options.length);
     const option = this.state.options[randomNum];
     alert(option);
   }
 
-  handleAddOption(option) {
+  handleAddOption = (option) => {
     if (!option) {
       return "Enter valid value to add item";
     } else if (this.state.options.indexOf(option) > -1) {
