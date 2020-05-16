57269e5459eccfe1e778b8d95cd409e006078e81
Winadi <winadiw@gmail.com>
2020-05-16 22:45:07 +0700

    #61 - add react-modal for popup modal

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -3,10 +3,12 @@ import AddOption from "./AddOption";
 import Options from "./Options";
 import Header from "./Header";
 import Action from "./Action";
+import OptionModal from "./OptionModal";
 
 export default class IndecisionApp extends React.Component {
   state = {
-    options: this.props.options
+    options: this.props.options,
+    selectedOption: undefined
   };
 
   componentDidMount() {
@@ -46,7 +48,15 @@ export default class IndecisionApp extends React.Component {
   handlePick = () => {
     const randomNum = Math.floor(Math.random() * this.state.options.length);
     const option = this.state.options[randomNum];
-    alert(option);
+    this.setState({
+      selectedOption: option
+    })
+  }
+
+  handleClearSelectedOption = () => {
+    this.setState({
+      selectedOption: undefined
+    })
   }
 
   handleAddOption = (option) => {
@@ -77,6 +87,7 @@ export default class IndecisionApp extends React.Component {
           handleDeleteOption={this.handleDeleteOption}
         />
         <AddOption handleAddOption={this.handleAddOption} />
+        <OptionModal selectedOption={this.state.selectedOption} handleClearSelectedOption={this.handleClearSelectedOption} />
       </div>
     );
   }
