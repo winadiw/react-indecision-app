2be9d7a62169f3ce89d2b7997dd3b4bb6f1d0269
Winadi <winadiw@gmail.com>
2020-05-17 12:39:55 +0700

    #67 - theming with variables, add container for nicer spacings

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -8,7 +8,7 @@ import OptionModal from "./OptionModal";
 export default class IndecisionApp extends React.Component {
   state = {
     options: this.props.options,
-    selectedOption: undefined
+    selectedOption: undefined,
   };
 
   componentDidMount() {
@@ -37,27 +37,27 @@ export default class IndecisionApp extends React.Component {
 
   handleDeleteOptions = () => {
     this.setState(() => ({ options: [] }));
-  }
+  };
 
   handleDeleteOption = (optionToRemove) => {
     this.setState((prevState) => ({
       options: prevState.options.filter((option) => optionToRemove !== option),
     }));
-  }
+  };
 
   handlePick = () => {
     const randomNum = Math.floor(Math.random() * this.state.options.length);
     const option = this.state.options[randomNum];
     this.setState({
-      selectedOption: option
-    })
-  }
+      selectedOption: option,
+    });
+  };
 
   handleClearSelectedOption = () => {
     this.setState({
-      selectedOption: undefined
-    })
-  }
+      selectedOption: undefined,
+    });
+  };
 
   handleAddOption = (option) => {
     if (!option) {
@@ -69,7 +69,7 @@ export default class IndecisionApp extends React.Component {
     this.setState((prevState) => ({
       options: prevState.options.concat(option),
     }));
-  }
+  };
 
   render() {
     const subtitle = "Put your life in the hands of a computer";
@@ -77,17 +77,23 @@ export default class IndecisionApp extends React.Component {
     return (
       <div>
         <Header subtitle={subtitle} />
-        <Action
-          hasOptions={this.state.options.length > 0}
-          handlePick={this.handlePick}
+        <div className="container">
+          <Action
+            hasOptions={this.state.options.length > 0}
+            handlePick={this.handlePick}
+          />
+          <Options
+            options={this.state.options}
+            handleDeleteOptions={this.handleDeleteOptions}
+            handleDeleteOption={this.handleDeleteOption}
+          />
+          <AddOption handleAddOption={this.handleAddOption} />
+        </div>
+
+        <OptionModal
+          selectedOption={this.state.selectedOption}
+          handleClearSelectedOption={this.handleClearSelectedOption}
         />
-        <Options
-          options={this.state.options}
-          handleDeleteOptions={this.handleDeleteOptions}
-          handleDeleteOption={this.handleDeleteOption}
-        />
-        <AddOption handleAddOption={this.handleAddOption} />
-        <OptionModal selectedOption={this.state.selectedOption} handleClearSelectedOption={this.handleClearSelectedOption} />
       </div>
     );
   }
